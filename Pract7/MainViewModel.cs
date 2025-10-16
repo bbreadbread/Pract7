b7f5fb6e63f83e6a7d0d26767c6b7834fa651458
bbreadbread <157108707+bbreadbread@users.noreply.github.com>
2025-10-16 05:25:29 +0500

    Update MainViewModel.cs

--- Pract7/MainViewModel.cs
+++ Pract7/MainViewModel.cs
@@ -24,7 +24,7 @@ namespace Pract7
         private string authId = string.Empty;
         private string searchId = string.Empty;
         public string AuthId { get { return authId; } set { authId = value; onPropertyChanged("AuthId"); } } 
-        public string SearchId { get => searchId; set { searchId = value; onPropertyChanged("SearchId"); } }
+        public string SearchId { get { return searchId; } set { searchId = value; onPropertyChanged("SearchId"); } }
         public MainViewModel()
         {
             Doctor = new Doctor();
